bfb9aba685578159fd4786b248e09e9f907bc95d
Kevin Black <kevin3.black@gmail.com>
2020-08-03 02:07:22 -0500

    Updates

---
+++ precache-manifest.f55c53400963b16c38077cd9ae5676ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c6c46f3486c9dcb636cb39cb72b5ab",
+    "url": "/opat-js/index.html"
+  },
+  {
+    "revision": "42f2a24c30463c8c39f9",
+    "url": "/opat-js/static/css/main.d337f220.chunk.css"
+  },
+  {
+    "revision": "0deaecd3995e0edbd314",
+    "url": "/opat-js/static/js/2.ab34cce9.chunk.js"
+  },
+  {
+    "revision": "d1f552577fbd0859131edd51f65f75d8",
+    "url": "/opat-js/static/js/2.ab34cce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f2a24c30463c8c39f9",
+    "url": "/opat-js/static/js/main.780f6d15.chunk.js"
+  },
+  {
+    "revision": "386dbc45b3b833a337c1",
+    "url": "/opat-js/static/js/runtime-main.17c417f5.js"
+  },
+  {
+    "revision": "7bb66b61107773d01e577fb1b008ee8e",
+    "url": "/opat-js/static/media/cygnus.7bb66b61.glb"
+  },
+  {
+    "revision": "1d777ccf4bb7c33a6ca09e2c353e85ee",
+    "url": "/opat-js/static/media/image_000.1d777ccf.png"
+  },
+  {
+    "revision": "097c432eacc4ae1d45944f1635dcd612",
+    "url": "/opat-js/static/media/image_240.097c432e.png"
+  },
+  {
+    "revision": "90688ae995cfe21a23a917c1721de074",
+    "url": "/opat-js/static/media/image_480.90688ae9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/opat-js/precache-manifest.0c24a4b461ed856853aaaca2a586084c.js"
+  "/opat-js/precache-manifest.f55c53400963b16c38077cd9ae5676ca.js"
 );
 
 self.addEventListener('message', (event) => {
